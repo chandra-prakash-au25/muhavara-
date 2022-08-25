1fba47b3b1d9f1ee0333dd80e7a99f3e1fa67125
Chandra-Prakash- au25 <87293840+chandra-prakash-au25@users.noreply.github.com>
2022-08-25 23:45:38 +0530

    dad

--- routes/product.js
+++ routes/product.js
@@ -82,5 +82,15 @@ router.put("/products/:cat", async (req, res) => {
     }
 });
 
+router.get("/products", async (req, res) => {
+    try {
+        const product = await Product.find();
+        
+        res.status(200).json(product);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 
 module.exports = router;
